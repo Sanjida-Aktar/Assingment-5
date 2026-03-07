5c03599174e7dac8da624f74c6ae6f4e181abc0e
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-03-07 14:05:27 +0600

    Add login section

---
+++ script/index.js
@@ -0,0 +1,24 @@
+
+
+function getValueFromInput(id){
+    const input = document.getElementById(id);
+    const value = input.value;
+    return value;
+};
+
+document.getElementById("login-btn").addEventListener("click",function(){
+const userName = getValueFromInput("user-name");
+
+    const pin= getValueFromInput("passward");
+
+    if(userName==="admin" && pin=== "admin123"){
+       
+             alert("successfully login");
+             window.location.href = "issue-traker.html";  
+             
+    }   
+    else{
+        alert("invalid username or pin");
+    }
+
+});
